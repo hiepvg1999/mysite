fb73b4ec60c97372eb97ed085632963fd7ae7e7a
hiepvg1999 <hiepbkcntt@gmail.com>
2021-08-03 09:47:08 +0700

    Django Tutorial Part 6: Generic list and detail views

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -24,3 +24,10 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
